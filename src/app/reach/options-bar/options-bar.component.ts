893ffde0aec81f695fa4e418dba1ca0238aafa71
andrewjfei <andrewjfei@gmail.com>
2020-05-18 20:34:07 +1200

    option buttons visuals working correctly

--- src/app/reach/options-bar/options-bar.component.ts
+++ src/app/reach/options-bar/options-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter, Input } from '@angular/core';
 
 @Component({
   selector: 'app-options-bar',
@@ -6,6 +6,7 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./options-bar.component.css']
 })
 export class OptionsBarComponent implements OnInit {
+  @Input() selectedNode: string;
   @Output() handleNodeChange: EventEmitter<String> = new EventEmitter();
 
   options: object = [

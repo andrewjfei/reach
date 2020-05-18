ff03e705c05c555b43fa3303cfbc0f4f1fc337c1
andrewjfei <andrewjfei@gmail.com>
2020-05-18 18:26:22 +1200

    added header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { GridComponent } from './reach/grid/grid.component';
 import { OptionsBarComponent } from './reach/options-bar/options-bar.component';
 import { ReachComponent } from './reach/reach.component';
+import { HeaderComponent } from './reach/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GridComponent,
     OptionsBarComponent,
-    ReachComponent
+    ReachComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
